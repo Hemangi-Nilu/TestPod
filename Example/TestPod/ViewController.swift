3871a415015792cfa602ac75a618d581cc3abbc6
Hemangi <hemangitrivedi.nilu@gmail.com>
2021-11-25 11:13:09 +0530

    initial commit

--- Example/TestPod/ViewController.swift
+++ Example/TestPod/ViewController.swift
@@ -7,17 +7,20 @@
 //
 
 import UIKit
+import Pods_TestPod_Example
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let loger = Logger()
+        loger.printLog1()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 }
---
+++ Logger.swift
@@ -0,0 +1,18 @@
+//
+//  Logger.swift
+//  Pods-TestPod_Example
+//
+//  Created by Mac Mini on 25/11/21.
+//
+
+import Foundation
+public class Logger{
+   public init(){}
+   public func printLog1(){
+        print("hello")
+    }
+    
+   func privateMethod(){
+        print("private")
+        }
+}
